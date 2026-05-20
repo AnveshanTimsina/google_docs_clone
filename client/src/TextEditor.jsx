e85576b8c358e713e6537d6d5e1382d1a3c980d1
Anveshan Timsina <timsinaanveshan@gmail.com>
2026-05-20 20:22:55 +0545

    implemented documentId, created rooms for each document

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -1,7 +1,8 @@
 import { useCallback, useEffect, useState } from 'react'
-import Quill, { Delta } from 'quill'
+import Quill from 'quill'
 import 'quill/dist/quill.snow.css'
 import {io} from "socket.io-client"
+import { useParams } from 'react-router-dom'
 
 const TOOLBAR_OPTIONS = [
   [{header: [1,2,3,4,5,6,false]}],
@@ -16,8 +17,23 @@ const TOOLBAR_OPTIONS = [
 ]
 
 export default function TextEditor() {
+  const {id: documentId} = useParams()
   const [socket, setSocket] = useState()
   const [quill, setQuill] = useState()
+
+  useEffect(()=>{
+    if (socket == null || quill == null) return
+
+    socket.once('load-document', document =>{
+      quill.setContents(document)
+      quill.enable()
+    })
+
+
+    socket.emit('get-document', documentId)
+
+
+  },[socket, quill, documentId])
 
   useEffect(()=>{
     const s = io("http://localhost:3001")
@@ -65,6 +81,8 @@ export default function TextEditor() {
     const editor = document.createElement('div')
     wrapper.append(editor)
     const q  = new Quill(editor, { theme: 'snow', modules : {toolbar:TOOLBAR_OPTIONS} })
+    q.disable()
+    q.setText("Loading...")
     setQuill(q)
   }, [])
 
