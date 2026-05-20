710ab578b3f71ce1aeb49bfbac63828368e1f26b
Anveshan Timsina <timsinaanveshan@gmail.com>
2026-05-21 00:54:23 +0545

    mongoose integration

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -4,6 +4,7 @@ import 'quill/dist/quill.snow.css'
 import {io} from "socket.io-client"
 import { useParams } from 'react-router-dom'
 
+const SAVE_INTERVAL_MS = 2000
 const TOOLBAR_OPTIONS = [
   [{header: [1,2,3,4,5,6,false]}],
   [{font:[]}],
@@ -24,14 +25,25 @@ export default function TextEditor() {
   useEffect(()=>{
     if (socket == null || quill == null) return
 
+    const interval = setInterval(()=>{
+      socket.emit('save-document', quill.getContents())
+
+    }, SAVE_INTERVAL_MS)
+
+    return () =>{
+      clearInterval(interval)
+    }
+  }, [socket,quill])
+
+  useEffect(()=>{
+    if (socket == null || quill == null) return
+
     socket.once('load-document', document =>{
       quill.setContents(document)
       quill.enable()
     })
 
-
     socket.emit('get-document', documentId)
-
 
   },[socket, quill, documentId])
 
@@ -41,7 +53,6 @@ export default function TextEditor() {
 
     return()=>{
       s.disconnect()
-
     }
   },[])
 
